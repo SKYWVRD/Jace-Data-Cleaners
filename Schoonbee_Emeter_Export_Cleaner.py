21c881d31903c0999cbd8c54c7ac1109d48bbe05
Sean Andrew Boonzaier <boonzaiersa@gmail.com>
2020-01-15 14:47:33 +0200

    Add files via upload

--- Schoonbee_Emeter_Export_Cleaner.py
+++ Schoonbee_Emeter_Export_Cleaner.py
@@ -4,6 +4,7 @@ in_file_name = "Eskom_Data.csv"
 out_file_name = "Eskom_Data_Sorted.csv"   
 
 def clean_data(in_file, out_file):
+    ''' Removes text that clashes with text to columns '''
     with open(in_file) as f_in:
         contents = f_in.read()
         contents = contents.replace(' AM CAT', '')
@@ -11,7 +12,9 @@ def clean_data(in_file, out_file):
         contents = contents.replace(' (A)', '')
         contents = contents.replace(' (V)', '')
         contents = contents.replace(',"', '')
+        #seperates timestamp to date, time to allow for two columns
         contents = contents.replace('timestamp', 'Date,Time')
+        #replaces spaces with commas to allow for date/time seperation
         contents = contents.replace(' ', ',')
         with open(out_file, 'w') as f_out:
             f_out.write(contents)
